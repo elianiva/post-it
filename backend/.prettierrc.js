468983041a263f121b9d266b6ee9ea21d5a18485
elianiva <dicha.arkana03@gmail.com>
2021-06-15 15:39:25 +0700

    chore: add prettier

---
+++ backend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  printWidth: 80,
+};
