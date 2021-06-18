4d5c3c09d9af95ce4e631db88d0d51e50c1525d0
elianiva <dicha.arkana03@gmail.com>
2021-06-18 08:04:49 +0700

    refactor: use function to make it easier to test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
-import dotenv from "dotenv";
-import Fastify, { FastifyInstance } from "fastify";
+import Fastify, { FastifyInstance, FastifyServerOptions } from "fastify";
 import fastifyCookie from "fastify-cookie";
 import { IncomingMessage, Server, ServerResponse } from "http";
 import { createConnection } from "typeorm";
@@ -8,66 +7,58 @@ import { User } from "./entities/User";
 import { AuthRoutes } from "./routes/Auth";
 import { PostRoutes } from "./routes/Post";
 
-dotenv.config();
-const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_PORT } = process.env;
+const PORT = 3000;
 
-export class App {
-  private PORT = 3000;
-  public server: FastifyInstance<Server, IncomingMessage, ServerResponse>;
+type ServerInstance = FastifyInstance<Server, IncomingMessage, ServerResponse>;
 
-  constructor(
-    server: FastifyInstance<Server, IncomingMessage, ServerResponse>
-  ) {
-    this.server = server;
-    (async () => {
-      // wait for the database to be ready
-      await this.connectDatabase();
-      this.registerPlugins();
-      this.registerRoutes();
-      this.init();
-    })();
+export const createServer = async (
+  opts: { testing?: boolean } & FastifyServerOptions
+): Promise<ServerInstance> => {
+  const server = Fastify(opts);
+
+  // connect to the database
+  try {
+    await createConnection({
+      type: "postgres",
+      host: DB_HOST,
+      database: DB_NAME,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      port: DB_PORT as unknown as number,
+      synchronize: true,
+      logging: false,
+      entities: [User, Post],
+    });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
   }
 
-  init(): void {
-    this.server
-      .listen(this.PORT)
-      .then(() => console.log(`Server started on port: ${this.PORT}`))
+  // register plugins
+  server.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET as string,
+  });
+
+  // register routes
+  server.get("/", async () => ({
+    status: 200,
+    msg: "Hello, World!",
+  }));
+  server.register(AuthRoutes, { prefix: "/api/auth" });
+  server.register(PostRoutes, { prefix: "/api/post" });
+
+  // listen to a port only if it's not in testing environment
+  if (!opts.testing) {
+    server
+      .listen(PORT)
+      .then(() => console.log(`Server started on port: ${PORT}`))
       .catch(err => {
-        this.server.log.error(err);
+        server.log.error(err);
         process.exit(1);
       });
   }
 
-  registerRoutes(): void {
-    this.server.get("/", async () => ({ hello: "world" }));
-
-    this.server.register(AuthRoutes, { prefix: "/api/auth" });
-    this.server.register(PostRoutes, { prefix: "/api/post" });
-  }
-
-  registerPlugins(): void {
-    this.server.register(fastifyCookie, {
-      secret: process.env.COOKIE_SECRET as string,
-    });
-  }
-
-  async connectDatabase(): Promise<void> {
-    try {
-      await createConnection({
-        type: "postgres",
-        host: DB_HOST,
-        database: DB_NAME,
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-        synchronize: true,
-        logging: false,
-        entities: [User, Post],
-      });
-    } catch (err) {
-      this.server.log.error(err);
-      process.exit(1);
-    }
-  }
-}
-
-new App(Fastify({ logger: true }));
+  // return the server instance
+  return server;
+};
