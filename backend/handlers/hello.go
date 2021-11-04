207674722355b20a441660e28fb872a378eb881b
elianiva <dicha.arkana03@gmail.com>
2021-11-04 12:06:56 +0700

    chore: sanity check

--- backend/handlers/hello.go
+++ backend/handlers/hello.go
@@ -2,6 +2,6 @@ package handlers
 
 import "github.com/gofiber/fiber/v2"
 
-func Hello(ctx *fiber.Ctx) error {
-	return ctx.SendString("Henlo")
+func (d *Dependency) Hello(ctx *fiber.Ctx) error {
+	return ctx.SendString("Hello, 世界！")
 }
